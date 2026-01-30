95dcc5ff803d8242b4fc86ce053502130e384ddc
davidpracilio <dave.pracilio@outlook.com>
2026-01-30 17:46:09 +0800

    Add more details to ai prompt

--- gemini_client.go
+++ gemini_client.go
@@ -203,6 +203,9 @@ Search Query: %s`, req.Query)
 
 	prompt += `
 
+Include venues that allow drop and leave activities. A booking url is preferred, but if one cannot be found,
+provide a link to the venue or organiser's main website as the booking url.
+
 Please respond with ONLY a JSON array of activities in the following format (no additional text):
 [
   {
